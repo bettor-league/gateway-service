1901ea4fec625eb99d058eb0219dc9e4bb65251e
cnadjim <c.nadjim@gmail.com>
2019-12-10 22:01:02 +0100

    delete zuul dependency

--- src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
+++ src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
@@ -3,11 +3,11 @@ package com.bettorleague.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+@EnableCircuitBreaker
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
