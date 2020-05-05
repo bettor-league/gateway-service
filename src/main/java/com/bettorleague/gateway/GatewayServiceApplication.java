bc0ff5a1646a230ad46bb3e621db794786392b04
cnadjim <c.nadjim@gmail.com>
2020-05-05 12:55:28 +0200

    maj

--- src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
+++ src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
@@ -1,8 +1,11 @@
 package com.bettorleague.gateway;
 
+import com.bettorleague.microservice.swagger.SwaggerMicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SwaggerMicroservice
 @SpringBootApplication
 public class GatewayServiceApplication {
 	public static void main(String[] args) {
