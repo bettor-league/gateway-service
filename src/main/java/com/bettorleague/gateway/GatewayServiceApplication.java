89452df047c3e2811ed6d98398f47148fbc436fc
Chabane Nadjim <c.nadjim@gmail.com>
2020-06-10 20:31:29 +0200

    Merge Develop

--- src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
+++ src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
@@ -1,15 +1,14 @@
 package com.bettorleague.gateway;
 
+import com.bettorleague.microservice.swagger.SwaggerMicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@SwaggerMicroservice
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
