64520a69daf9e633b1375141ed506791e9c18236
cnadjim <c.nadjim@gmail.com>
2023-09-20 20:04:56 +0200

    bump spring 3

--- src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
+++ src/main/java/com/bettorleague/gateway/GatewayServiceApplication.java
@@ -1,12 +1,15 @@
 package com.bettorleague.gateway;
 
 import com.bettorleague.microservice.swagger.SwaggerMicroservice;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SwaggerMicroservice
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gateway API", version = "1.0", description = "Documentation Gateway API v1.0"))
 public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
---
+++ src/main/java/com/bettorleague/gateway/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.bettorleague.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        //configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
